541f793167dfa16caae3ef63a1c54117d281e35f
Paul Colomiets <paul@colomiets.name>
2018-03-27 15:39:52 +0300

    Allow replacing listen directive

--- src/bin/nginx_config_mod/modify.rs
+++ src/bin/nginx_config_mod/modify.rs
@@ -2,10 +2,11 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 
 use nginx_config_mod::{Config, EntryPoint};
-use nginx_config::visitors::{replace_vars};
+use nginx_config::parse_directives;
+use nginx_config::ast::{self, Listen};
+use nginx_config::visitors::{replace_vars, visit_mutable};
 
 use failure::Error;
-
 
 #[derive(StructOpt)]
 pub struct Modify {
@@ -16,6 +17,23 @@ pub struct Modify {
     #[structopt(short="s", long="subst-variable", name="var=value",
                 help="replace variable in the config to specified value")]
     set_var: Vec<String>,
+    #[structopt(long="listen", name="LISTEN",
+                help="replace all listen directives to this value",
+                parse(try_from_str="parse_listen"))]
+    listen: Option<Listen>,
+}
+
+fn parse_listen(s: &str) -> Result<Listen, Error> {
+    let text = format!("listen {};", s);
+    let mut dirs = parse_directives(&text)?;
+    if dirs.len() > 1 {
+        bail!("Only single listen directive may be specified \
+               (consider removing semicolon from argument)");
+    }
+    match dirs.pop().map(|d| d.item) {
+        Some(ast::Item::Listen(lst)) => Ok(lst),
+        _ => bail!("Internal error when parsing listen directive"),
+    }
 }
 
 
@@ -28,6 +46,14 @@ pub fn run(modify: Modify) -> Result<(), Error> {
             pair.next().unwrap_or(""));
     }
     replace_vars(cfg.directives_mut(), |name| vars.get(name).map(|x| *x));
+    if let Some(new_listen) = modify.listen {
+        visit_mutable(cfg.directives_mut(), |dir| {
+            match dir.item {
+                ast::Item::Listen(ref mut lst) => *lst = new_listen.clone(),
+                _ => {}
+            }
+        });
+    }
     print!("{}", cfg);
     Ok(())
 }

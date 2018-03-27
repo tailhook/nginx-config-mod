8838421cc9c9985eb50a2a60213810777dd706f2
Paul Colomiets <paul@colomiets.name>
2018-03-27 15:44:28 +0300

    Improve docstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Nginx Config Modification Tool
+//! ==============================
+//!
+//! Note: we currently working on command-line tool, so internals/lib are
+//! poorly documented. We plan to improve on it later.
+//!
+//! [Docs](https://docs.rs/nginx-config-mod/) |
+//! [Github](https://github.com/tailhook/nginx-config-mod/) |
+//! [Crate](https://crates.io/crates/nginx-config-mod)
+//!
 extern crate nginx_config;
 #[macro_use] extern crate failure;
 
